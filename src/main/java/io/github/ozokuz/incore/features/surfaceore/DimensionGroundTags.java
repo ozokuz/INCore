ccafec5d7bafec85e9a864f2b17a0ed6840e3f43
Ozoku <19756856+ozokuz@users.noreply.github.com>
2026-02-25 12:10:37 +0200

    nether stone patches

---
+++ src/main/java/io/github/ozokuz/incore/features/surfaceore/DimensionGroundTags.java
@@ -0,0 +1,79 @@
+package io.github.ozokuz.incore.features.surfaceore;
+
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+
+public final class DimensionGroundTags {
+    private DimensionGroundTags() {}
+
+    public static boolean isNaturalGround(BlockState state, ResourceKey<Level> dimension) {
+        DimensionCategory category = DimensionCategory.fromLevel(dimension);
+        
+        return switch (category) {
+            case OVERWORLD -> isOverworldGround(state);
+            case NETHER -> isNetherGround(state);
+            case END -> isEndGround(state);
+        };
+    }
+
+    public static boolean isSoftVegetation(BlockState state, ResourceKey<Level> dimension) {
+        DimensionCategory category = DimensionCategory.fromLevel(dimension);
+        
+        return switch (category) {
+            case OVERWORLD -> isOverworldVegetation(state);
+            case NETHER -> isNetherVegetation(state);
+            case END -> false;
+        };
+    }
+
+    private static boolean isOverworldGround(BlockState state) {
+        return state.is(BlockTags.BASE_STONE_OVERWORLD)
+                || state.is(BlockTags.DIRT)
+                || state.is(BlockTags.SAND)
+                || state.is(BlockTags.TERRACOTTA)
+                || state.is(Blocks.GRAVEL)
+                || state.is(Blocks.CLAY)
+                || state.is(Blocks.SNOW_BLOCK)
+                || state.is(Blocks.POWDER_SNOW)
+                || state.is(Blocks.STONE)
+                || state.is(Blocks.STONE_SLAB);
+    }
+
+    private static boolean isNetherGround(BlockState state) {
+        return state.is(BlockTags.BASE_STONE_NETHER)
+                || state.is(Blocks.NETHERRACK)
+                || state.is(Blocks.SOUL_SAND)
+                || state.is(Blocks.SOUL_SOIL)
+                || state.is(Blocks.BASALT)
+                || state.is(Blocks.WARPED_NYLIUM)
+                || state.is(Blocks.CRIMSON_NYLIUM)
+                || state.is(Blocks.GRAVEL);
+    }
+
+    private static boolean isEndGround(BlockState state) {
+        return state.is(Blocks.END_STONE);
+    }
+
+    private static boolean isOverworldVegetation(BlockState state) {
+        return state.is(Blocks.SHORT_GRASS)
+                || state.is(Blocks.TALL_GRASS)
+                || state.is(Blocks.FERN)
+                || state.is(Blocks.LARGE_FERN)
+                || state.is(Blocks.DEAD_BUSH)
+                || state.is(BlockTags.SMALL_FLOWERS)
+                || state.is(BlockTags.TALL_FLOWERS);
+    }
+
+    private static boolean isNetherVegetation(BlockState state) {
+        return state.is(Blocks.NETHER_SPROUTS)
+                || state.is(Blocks.TWISTING_VINES)
+                || state.is(Blocks.TWISTING_VINES_PLANT)
+                || state.is(Blocks.WEEPING_VINES)
+                || state.is(Blocks.WEEPING_VINES_PLANT)
+                || state.is(Blocks.CRIMSON_ROOTS)
+                || state.is(Blocks.WARPED_ROOTS);
+    }
+}
--- src/main/java/io/github/ozokuz/incore/features/surfaceore/SurfaceOreType.java
+++ src/main/java/io/github/ozokuz/incore/features/surfaceore/SurfaceOreType.java
@@ -13,29 +13,49 @@ import net.minecraft.world.level.block.SlabBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+
 public enum SurfaceOreType implements StringRepresentable {
-    CRIMSITE("crimsite", "create:crimsite", "create:cut_crimsite_slab"),
-    VERIDIUM("veridium", "create:veridium", "create:cut_veridium_slab"),
-    ASURINE("asurine", "create:asurine", "create:cut_asurine_slab"),
-    OCHRUM("ochrum", "create:ochrum", "create:cut_ochrum_slab"),
-    CINNABAR("cinnabar", "incore:cinnabar_ore_stone", "incore:cinnabar_ore_stone_slab"),
-    MIXED_METALS("mixed_metals", "incore:mixed_metals_ore_stone", "incore:mixed_metals_ore_stone_slab"),
-    GEM_CLUSTERS("gem_clusters", "incore:gem_clusters_ore_stone", "incore:gem_clusters_ore_stone_slab");
+    CRIMSITE("crimsite", "create:crimsite", "create:cut_crimsite_slab", DimensionCategory.OVERWORLD),
+    VERIDIUM("veridium", "create:veridium", "create:cut_veridium_slab", DimensionCategory.OVERWORLD),
+    ASURINE("asurine", "create:asurine", "create:cut_asurine_slab", DimensionCategory.OVERWORLD),
+    OCHRUM("ochrum", "create:ochrum", "create:cut_ochrum_slab", DimensionCategory.OVERWORLD),
+    CINNABAR("cinnabar", "incore:cinnabar_ore_stone", "incore:cinnabar_ore_stone_slab", DimensionCategory.OVERWORLD),
+    MIXED_METALS("mixed_metals", "incore:mixed_metals_ore_stone", "incore:mixed_metals_ore_stone_slab", DimensionCategory.OVERWORLD),
+    GEM_CLUSTERS("gem_clusters", "incore:gem_clusters_ore_stone", "incore:gem_clusters_ore_stone_slab", DimensionCategory.OVERWORLD);
 
     private static final SurfaceOreType[] VALUES = values();
 
     private final String serializedName;
     private final ResourceLocation oreStoneBlockId;
     private final ResourceLocation oreStoneSlabId;
+    private final DimensionCategory dimension;
 
-    SurfaceOreType(String serializedName, String oreStoneBlockId, String oreStoneSlabId) {
+    SurfaceOreType(String serializedName, String oreStoneBlockId, String oreStoneSlabId, DimensionCategory dimension) {
         this.serializedName = serializedName;
         this.oreStoneBlockId = ResourceLocation.parse(oreStoneBlockId);
         this.oreStoneSlabId = ResourceLocation.parse(oreStoneSlabId);
+        this.dimension = dimension;
     }
 
     public static SurfaceOreType random(RandomSource random) {
         return VALUES[random.nextInt(VALUES.length)];
+    }
+
+    public static SurfaceOreType random(RandomSource random, DimensionCategory category) {
+        SurfaceOreType[] filtered = Arrays.stream(VALUES)
+                .filter(type -> type.dimension == category)
+                .toArray(SurfaceOreType[]::new);
+        
+        if (filtered.length == 0) {
+            return null;
+        }
+        
+        return filtered[random.nextInt(filtered.length)];
+    }
+
+    public DimensionCategory getDimension() {
+        return dimension;
     }
 
     public ItemStack oreDropStack() {
