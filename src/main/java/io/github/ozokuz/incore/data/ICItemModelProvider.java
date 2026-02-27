2fe1a1f9fa7fa42ccf8f4ec4e856a47e5b4c7945
Ozoku <19756856+ozokuz@users.noreply.github.com>
2026-02-27 04:54:45 +0200

    work on textures 1

--- src/main/java/io/github/ozokuz/incore/data/ICItemModelProvider.java
+++ src/main/java/io/github/ozokuz/incore/data/ICItemModelProvider.java
@@ -14,10 +14,28 @@ public class ICItemModelProvider extends ItemModelProvider {
     @Override
     protected void registerModels() {
         basicItem(Registration.ENCOUNTER_WAND_ITEM.get());
-        basicItem(Registration.SANITY_CRATE_ITEM.get());
+
         basicItem(Registration.SANITY_BOOSTER_SMALL_ITEM.get());
         basicItem(Registration.SANITY_BOOSTER_LARGE_ITEM.get());
         basicItem(Registration.SANITY_VESSEL_ITEM.get());
-        basicItem(Registration.BATTLEPASS_LANE_UNLOCK_ITEM.get());
+
+        //basicItem(Registration.SANITY_CRATE_ITEM.get());
+        //basicItem(Registration.BATTLEPASS_LANE_UNLOCK_ITEM.get());
+
+//        basicItem(Registration.CRIMSITE_ORE_LOCATOR.get());
+//        basicItem(Registration.VERIDIUM_ORE_LOCATOR.get());
+//        basicItem(Registration.ASURINE_ORE_LOCATOR.get());
+//        basicItem(Registration.OCHRUM_ORE_LOCATOR.get());
+//        basicItem(Registration.CINNABAR_ORE_LOCATOR.get());
+//        basicItem(Registration.MIXED_METALS_ORE_LOCATOR.get());
+//        basicItem(Registration.GEM_CLUSTERS_ORE_LOCATOR.get());
+//        basicItem(Registration.NETHER_QUARTZ_ORE_LOCATOR.get());
+//        basicItem(Registration.STONE_LOCATOR.get());
+//        basicItem(Registration.DEEPSLATE_LOCATOR.get());
+//        basicItem(Registration.LIMESTONE_LOCATOR.get());
+//        basicItem(Registration.BASALT_LOCATOR.get());
+//        basicItem(Registration.SCORIA_LOCATOR.get());
+        basicItem(Registration.UNIVERSAL_ORE_LOCATOR.get());
+        basicItem(Registration.UNIVERSAL_STONE_LOCATOR.get());
     }
 }
