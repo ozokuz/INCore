464badc40fa728e7df32653ba5bf4bef7455343e
Ozoku <19756856+ozokuz@users.noreply.github.com>
2026-03-02 16:02:41 +0200

    fix warning

--- src/main/java/io/github/ozokuz/incore/features/surfaceore/SurfaceOreDebugCompassClientEvents.java
+++ src/main/java/io/github/ozokuz/incore/features/surfaceore/SurfaceOreDebugCompassClientEvents.java
@@ -13,7 +13,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 
-@EventBusSubscriber(modid = INCore.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@EventBusSubscriber(modid = INCore.MODID, value = Dist.CLIENT)
 public final class SurfaceOreDebugCompassClientEvents {
     private SurfaceOreDebugCompassClientEvents() {
     }
